7a358b8fc445cfad3ddeef2030ffb267e3a77340
Saied.S <saied.suroth@gmail.com>
2019-12-21 00:42:09 +0100

    fichier csv corrigé + map initialisé (3/4)
    fichier en lecture csv corrigé (format bon) + map initialisé à 3/4 (manque à le mettre dans une variable matplotlib + mis des marqueurs selon les coordonnées depart de la bdd

--- map.py
+++ map.py
@@ -2,17 +2,17 @@ import mysql.connector
 from mysql.connector import Error
 from mysql.connector import connection
 
-"""from datetime import datetime, timedelta
-import matplotlib.pyplot as plt
-import cartopy.crs as ccrs
-import pandas as pd"""
+
 
 import os #si jamais ya un probleme avec BaseMap aller dans le chemin generalement dans programData: Anaconda3/Lib/site-packages/mpl_toolkits/basemap ensuite cree un dossier data puis mettre "epsg" dans ce dossier (sur discord)
 os.environ['PROJ_LIB'] = 'C:\\ProgramData\\Anaconda3\\Lib\\site-packages\\mpl_toolkits\\basemap\\data'
+
+import matplotlib.animation as animation
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-from mpl_toolkits.basemap import Basemap
+import folium
+from folium import plugins
 
 
 try:
@@ -28,6 +28,7 @@ try:
         rows = cursor.fetchall()
         
         fichier = open("test.csv","w") #récuperer les données de la bdd et les mettres dans un fichier .csv (si jamais on arrive pas a utiliser les donnees a partir d'une bdd alors on met les donnees de la bdd dans un fichier, c'est plus facile a manier enfin mon avis)
+        fichier.write("id;lat_dep;lng_dep;adresse_dep;lat_arr;lng_arr;adresse_arr;date_heur;id_user;etat\n")
         for row in rows:
             fichier.write(str(row[0])+";")
             fichier.write(str(row[1])+";")
@@ -41,36 +42,33 @@ try:
             fichier.write(str(row[9])+"\n")
             #Affichage de la table trouver
             print("L'id de la personne : {0},\nPlace départ -> coordonnée départ: [{1};{2}]-> {3}.\nPlace trouvée -> coordonnée arrivée: [{4};{5}]-> {6} à {7}\n".format(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7]))
+            
+        fichier.close()#fermer le fichier sinon le reste du coup ne fonctionnera pas       
         
         #lire le ficher .csv et mettre dans la variable df
-        #df = pd.read_csv('test.csv', names=['id', 'lat_dep', 'lng_dep', 'adresse_dep', 'lat_arr', 'lng_arr', 'adresse_arr', 'date_heur', 'id_user', 'etat'])
+        df = pd.read_csv("test.csv", sep=';', encoding='latin-1') 
+        #Voir ce que contient df
+        print(df)
+
         
-        fig=plt.figure()
+        
+        # on met les coordonnées de depart (pour test)
+        points = (df.lat_dep.fillna(0),df.lng_dep.fillna(0))
+        coordinates =[]
+        # On met les coordonnées de dep
+        lat = points[0]
+        long = points[1]
+        # Map de paris
+        m = folium.Map(location=[48.860419, 2.345341], tiles="cartodbpositron", zoom_start=12, width=900, height=600)
+        #la ici ça doit l'afficher mais ça le fait pas (jsp encore pq)
+        m
+        # lat_dep et lng_dep de la bdd test pour faire des points
+        for la,lo in zip(lat,long):
+            coordinates.append([la,lo])
 
-        #Initialisation de la map qui faudra mettre en folium au lieu de basemap.
-        bmap=Basemap(projection='merc', llcrnrlat=17.5,urcrnrlat=19.,llcrnrlon=-67.5, urcrnrlon=-65, epsg=4139)
-        bmap.arcgisimage(service='World_Shaded_Relief', xpixels = 2000)
-        plt.title("Test animation avec une map en background")
-
-        #Generer 100 points
-        N = 100
-        lon = np.linspace(-64, -68, N)
-        lat = np.linspace(17, 19.5, N)
-
-        #convertir pour avoir des points de coordonnées    
-        x,y = bmap(lon, lat)
-
-        #generer une ligne d'origine avec un seulement point 
-        line, = bmap.plot(x[0], y[0], linewidth = 1, color = 'm')
-
-        def animate(i):
-            #A chaque fois que cette fonctione Animate est appelé la ligne s'actualise point par point
-            line.set_data(x[:i],y[:i])  
-            return line
-        #pour l'animation
-        anim=animation.FuncAnimation(fig, animate, frames=N, interval=N)
-        #afficher la map et les points
-        plt.show()
+        m.add_child(plugins.FastMarkerCluster(coordinates[0:40000])) #ici pour que les points qu'on a ajouté soit en forme de marqueur
+        m #pareille ça doit afficher mais ca fait rien
+        m.save('index.html') #du coup je save en html et j'ouvre l'html
 
 except Error as e:
     print("Probleme de connexion à la base de données.", e)
