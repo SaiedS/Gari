cfaab42b63899d4a9444fc4c8ef9b84a830a55dc
Saied.S <saied.suroth@gmail.com>
2019-12-21 20:17:25 +0100

    maj de la bdd
    implementation table donner (faut rajouter de vraies info par contre)

--- testBdd.sql
+++ testBdd.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS trouver,donner,voitures;
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
@@ -24,6 +26,25 @@ INSERT INTO `trouver` (`id`, `lat_dep`, `lng_dep`, `adresse_dep`, `lat_arr`, `ln
 (5, 48.844017, 2.382108, '133 Avenue Daumesnil, 75012 Paris', 48.836328, 2.326721, '47 Rue Froidevaux, 75014 Paris', '2019-12-11 05:56:35', 6, 1),
 (6, 48.836328, 2.326721, '47 Rue Froidevaux, 75014 Paris', 48.857998, 2.300115, '52 Avenue Rapp, 75007 Paris', '2019-12-12 09:09:09', 7, 2);
 
+CREATE TABLE `donner` (
+  `id` bigint(64) UNSIGNED NOT NULL,
+  `lat` double NOT NULL,
+  `lng` double NOT NULL,
+  `adresse` varchar(255) NOT NULL,
+  `date_heur` datetime NOT NULL,
+  `heur_liberer` datetime NOT NULL,
+  `id_user` bigint(64) UNSIGNED NOT NULL,
+  `etat` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `donner` (`id`, `lat`, `lng`, `adresse`, `date_heur`, `heur_liberer`, `id_user`, `etat`) VALUES
+(1, 48.8774604, 2.3436713, '32 Rue de Maubeuge, 75009 Paris, France', '2019-12-18 22:29:31', '2019-12-18 22:29:31', 17, 1),
+(2, 48.834516, 2.331960, '10 Place Denfert-Rochereau P', '2019-12-10 03:00:43', '2019-12-10 03:00:43', 2, 2),
+(3, 48.84619741239477, 2.3512809164844, '59 Rue du Cardinal Lemoine,Paris, France, 75005','2019-12-18 22:38:30', '2019-12-18 22:38:30', 22, 1),
+(4, 48.8774054, 2.3436725, '32 Rue de Maubeuge, 75009 Paris, France', '2019-12-18 22:39:44', '2019-12-18 22:39:44', 17, 1),
+(5, 48.8774078, 2.3436731, '32 Rue de Maubeuge, 75009 Paris, France', '2019-12-18 22:40:25', '2019-12-18 22:40:25', 17, 1),
+(6, 48.8775908, 2.3434439, '32-34 Rue de Maubeuge, 75009 Paris, France', '2019-12-18 22:54:02', '2019-12-18 22:54:02', 22, 2),
+(7, 48.877411, 2.3436763, '32 Rue de Maubeuge, 75009 Paris, France', '2019-12-18 22:54:05', '2019-12-18 22:54:05', 17, 2);
 
 CREATE TABLE `voitures` (
   `id` bigint(64) UNSIGNED NOT NULL,
@@ -54,12 +75,15 @@ ALTER TABLE `trouver`
 ALTER TABLE `voitures`
   ADD PRIMARY KEY (`id`);
 
+ALTER TABLE `donner`
+  ADD PRIMARY KEY (`id`);
+  
+ALTER TABLE `donner`
+  MODIFY `id` bigint(64) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13813;
+
 ALTER TABLE `trouver`
   MODIFY `id` bigint(64) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3428;
   
 ALTER TABLE `voitures`
   MODIFY `id` bigint(64) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7507;
 COMMIT;
-
-
-
